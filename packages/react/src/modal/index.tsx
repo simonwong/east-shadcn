7fa7d7b5e0a902332be36537ecb6eb9f453b3442
Simon <wsj_simon@163.com>
2024-09-04 10:28:52 +0800

    feat: add Modal

--- packages/react/src/modal/index.tsx
+++ packages/react/src/modal/index.tsx
@@ -1,2 +1,3 @@
+export { Modal, type ModalProps } from './modal'
 export { AlertModal, type AlertModalProps } from './alert-modal'
 export { AlertModalHost, alertModalAction } from './alert-modal-host'
