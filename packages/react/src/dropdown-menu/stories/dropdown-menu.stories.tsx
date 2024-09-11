88ad0bd08f4981d6221225af18675864c7c965aa
Simon <wsj_simon@163.com>
2024-09-11 10:52:54 +0800

    feat: Tooltip add default style, default delayDuration

--- packages/react/src/dropdown-menu/stories/dropdown-menu.stories.tsx
+++ packages/react/src/dropdown-menu/stories/dropdown-menu.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { GithubIcon, HandshakeIcon, SuperscriptIcon, Webhook } from 'lucide-react'
+import { GithubIcon, HandshakeIcon, Webhook } from 'lucide-react'
 import { DropdownMenu } from '../index';
 
 const meta = {
