792ca0ecf7ba10d3a30b21429cc9d2c44257b10d
Simon <wsj_simon@163.com>
2024-09-26 18:51:22 +0800

    feat: add useModalAction for update modal props

---
+++ packages/react/src/modal/use-modal-action.ts
@@ -0,0 +1,65 @@
+import { useEffect, useRef } from "react"
+import { modalAction } from "./modal-host"
+import { ModalProps } from "./modal"
+import { AlertModalProps } from "./alert-modal"
+
+
+export const useModalAction = (props: ModalProps | AlertModalProps, dependencies: any[] = []) => {
+  const modalRef = useRef<{
+    modalInstance: {
+      update: (props: ModalProps | AlertModalProps) => void
+    },
+    initProps?: ModalProps | AlertModalProps
+  } | null>(null)
+
+  useEffect(() => {
+    if (modalRef.current) {
+      // props priority
+      modalRef.current.modalInstance.update({
+        ...modalRef.current.initProps,
+        ...props
+      })
+    }
+  }, dependencies)
+
+  // modalProps priority
+  const hookModalAction: typeof modalAction = {
+    open: (modalProps?: ModalProps) => {
+      const modalIns = modalAction.open({
+        ...props,
+        ...modalProps,
+      })
+      modalRef.current = {
+        modalInstance: modalIns,
+        initProps: modalProps
+      }
+      return modalIns
+    },
+    alert: (modalProps?: AlertModalProps) => {
+      const modalIns = modalAction.alert({
+        ...props,
+        ...modalProps,
+      })
+      modalRef.current = {
+        modalInstance: modalIns,
+        initProps: modalProps
+      }
+      return modalIns
+    },
+    confirm: (modalProps?: AlertModalProps) => {
+      const modalIns = modalAction.confirm({
+        ...props,
+        ...modalProps,
+      })
+      modalRef.current = {
+        modalInstance: modalIns,
+        initProps: modalProps
+      }
+      return modalIns
+    },
+  }
+
+  return [
+    hookModalAction,
+  ]
+}
